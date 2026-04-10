576ab04d8127af0ccedc45871712c30dafc88f6f
Ovpv1 <isaqueslb26@edu.unifor.br>
2026-04-10 20:47:44 -0300

    Improve input validation and game flow
    - Bugs about invalid characters now fixed
    - Code optimized and better structured

--- bingo.py
+++ bingo.py
@@ -1,42 +1,69 @@
-
 # Vou fazer um bingo de apenas uma letra. No caso, será a letra B, então vou criar uma lista apenas com a letra B, contendo 5 números
 import random
 
 
 # Essas são as listas que serão utilizadas ao longo do código 
 suaCartela = random.sample(range(1,11), 5)
-sorteado_na_cartela = [] # Essa, por via das dúvidas, mostrará os números que foram sorteados e que estão dentro da cartela do usuário.
+sorteado_na_cartela = [] # Essa (explicando por via das dúvidas), mostrará os números que foram sorteados e que estão dentro da cartela do usuário.
 numerosSorteados = []
 
 
-
 print('\n   Esta é a sua cartela de números: ', suaCartela)
-prosseguir = input('Devo prosseguir com o sorteio? (Responda "S" ou "N") ').upper()
+
+while(True):
+     prosseguir = input('Devo prosseguir com o sorteio? (Responda "S" ou "N") ').upper()
+     if(prosseguir != 'S' and prosseguir != 'N'):
+          print('Caractere inválido. Digite novamente.')
+     else:
+          break
 
 
-while(prosseguir == 'S' and len(sorteado_na_cartela) < 5):
-    sorteio = random.randint(1,10)
-    while(sorteio in numerosSorteados):
-         sorteio = random.randint(1,10)
-    numerosSorteados.append(sorteio)
+if(prosseguir == 'N'):
+     print('Bingo encerrado. \n')
+
+else:
+    
+    #O bingo em si inicia aqui
+     while(len(sorteado_na_cartela) < 5):
+
+          sorteio = random.randint(1,10)
+
+          while(sorteio in numerosSorteados): #Verifica se o número já foi sorteado anteriormente.
+               sorteio = random.randint(1,10)
+
+          numerosSorteados.append(sorteio)
+
+          #Adiciona o número na cartela, caso esteja nela.
+          if(sorteio in suaCartela):
+               sorteado_na_cartela.append(sorteio)
+               print(f'\n     O número sorteado foi: {sorteio}, e ele está em sua cartela!\n')
+          else:
+               print(f'\n     O número sorteado foi: {sorteio}, e ele não está em sua cartela!\n')
 
 
-    if(sorteio in suaCartela):
-         sorteado_na_cartela.append(sorteio)
-         print(f'\n     O número sorteado foi: {sorteio}, e ele está em sua cartela!\n ')
-         
-    else:
-         print(f'\n     O número sorteado foi: {sorteio}, e ele não está em sua cartela!\n')
-         
-    if len(sorteado_na_cartela) == len(suaCartela) - 2:
-         print(f'Faltam mais 2 números!')
-    if len(sorteado_na_cartela) == len(suaCartela) - 1:
-         print(f'FALTA SÓ MAIS 1 NÚMERO!')
+          #Aqui vão alguns avisos para alertar o jogador sobre quantos números faltam.
+          if(len(sorteado_na_cartela) == 3):
+               print(f'Faltam mais 2 números!')
+          elif(len(sorteado_na_cartela) == 4):
+               print(f'FALTA SÓ MAIS 1 NÚMERO!')
 
 
-    if len(sorteado_na_cartela) != 5:
-        prosseguir = input('Devo prosseguir com o sorteio? (Responda "S" ou "N") ').upper()
-    else:
-         prosseguir = 'N'
-         print('\n \n BINGO! Você bateu!\n \n')
+          #Verifica se a cartela foi preenchida ou não
+          if(len(sorteado_na_cartela) == 5):
+               print('\n \n BINGO! Você bateu! \n \n')
+               break
+          
 
+          while(True):
+
+               prosseguir = input('Devo prosseguir com o sorteio? (Responda "S" ou "N") ').upper()
+               if(prosseguir != 'N' and prosseguir != 'S'):
+                    print('Caractere inválido. Digite novamente.')
+               else:
+                    break
+
+          if(prosseguir == 'N'):
+               break
+
+
+closeApp = input('Pressione Enter para fechar o app')
